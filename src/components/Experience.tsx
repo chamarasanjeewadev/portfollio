8cd406bd4fde85ddd58ab4141e25414aaeb03d50
Chamara Sanjeewa <chamara.sanjeewa.uk@gmail.com>
2024-04-27 11:33:05 +0100

    wip: enabled portfollio toggle

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { BulletText } from '../atoms/BulletText';
 import { TitleText } from '../atoms/Title';
 import careerInfo from './../assets/portfollio.json';
 import { FaExternalLinkAlt } from 'react-icons/fa';
+import clsx from 'clsx';
+import { cn } from '../utils/cnUtil';
 
 export const Experience = () => {
   const carrier: SkillCardProps[] = careerInfo.career;
@@ -12,13 +15,20 @@ export const Experience = () => {
     <section id="experience" className="min-h-screen">
       <div className="flex content-center justify-between gap-2">
         <TitleText text="Experience" />
-        <div className=" flex items-center gap-2 ">
+        {/* <div className=" flex items-center gap-2 ">
           <h3>View resume </h3>
           <FaExternalLinkAlt
             className="cursor-pointer"
             onClick={() => window.open('/resume_chamara_sanjeewa.pdf')}
           />
-        </div>
+        </div> */}
+        <a
+          className="... text-sky-400 after:content-['_↗']"
+          href="/resume_chamara_sanjeewa.pdf"
+          target="_blank"
+        >
+          View Resume
+        </a>
       </div>
       <div className="mt-2 grid grid-cols-3 ">
         <div className="item1 col-span-1 ">
@@ -68,6 +78,7 @@ export const SkillCard = ({
   jd,
   tech,
 }: SkillCardProps) => {
+  const [showAll, setShowAll] = useState(false);
   return (
     <div className="relative  flex flex-col gap-2 text-wrap text-left md:group-hover:bg-slate-800/50">
       <div className="flex items-center gap-2 ">
@@ -84,27 +95,32 @@ export const SkillCard = ({
         <h2>{location}</h2>
       </div>
       <div
-        className="relative before:absolute  before:left-[-1.5rem]  before:top-0  before:cursor-pointer  before:text-light before:content-['▼▶']"
-        onClick={() => window.open('rootcode.io')}
+        className={cn(
+          "relative before:absolute  before:left-[-1.5rem]  before:top-0  before:cursor-pointer  before:text-light before:content-['▶']",
+          { "before:content-['▼']": showAll },
+        )}
+        onClick={() => setShowAll(x => !x)}
       >
         <p>
           <ul>
-            {[jd?.[0]].map((item, index) => (
+            {(!showAll ? [jd?.[0]] : [...jd]).map((item, index) => (
               <li key={index} className=" ">
-                {/* <a
-                className="text-sky-400 after:content-['_↗'] ..."
-                href="https://www.
-apple.com/pro-display-xdr/"
-                target="_blank"
-              ></a> */}
-                <p className="truncate text-main  ">{item}</p>
+                <p
+                  // className={cn(' truncate text-wrap text-main ', {
+                  //   truncate: showAll,
+                  // })}
+                  className={cn(' text-main flex-wrap ', {
+                    'truncate ': !showAll,
+                  })}
+                >
+                  {item}
+                </p>
               </li>
             ))}
           </ul>
         </p>
         <h1>Technologies & Tools</h1>
         <div className="flex flex-wrap gap-2">
-          
           {tech.map((item, index) => (
             <TechTag key={index} technology={item} />
           ))}
