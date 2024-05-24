61e0ad58f450a4e63389b7e8ba4a369f01561fa9
Chamara Sanjeewa <chamara.sanjeewa.uk@gmail.com>
2024-05-24 11:24:25 +0100

    minor change

--- src/services/portfollioService.ts
+++ src/services/portfollioService.ts
@@ -1,6 +1,6 @@
 export const getPortfolio = async () => {
   const response = await fetch(
-    'https://j3w51zasak.execute-api.us-east-1.amazonaws.com/',
+    'https://api.chamarasanjeewa.info/',
   );
   const data = await response.json();
   return data;
